761ec0686d54ae67d9fdd76a636ce33b7d1fbfff
Edmond Chuc <edmond@kurrawong.ai>
2025-08-29 15:01:37 +1000

    chore: improve types for createVocEditMachine

--- src/composables/vocedit-machine.ts
+++ src/composables/vocedit-machine.ts
@@ -1,13 +1,15 @@
 import { provide, inject } from 'vue'
 import { useMachine } from '@xstate/vue'
 import { voceditMachine } from '@/vocedit-machine'
+import type { CreateResourceManagerReturn } from '@/types'
 
-type UseMachineReturn = ReturnType<typeof useMachine<typeof voceditMachine>>
+type VocEditMachine = ReturnType<typeof voceditMachine>
+type UseMachineReturn = ReturnType<typeof useMachine<VocEditMachine>>
 
 const VOCEDIT_MACHINE_KEY = Symbol('vocedit-machine')
 
-export function createVocEditMachine() {
-  const machine = useMachine(voceditMachine)
+export function createVocEditMachine(resourceManager: CreateResourceManagerReturn) {
+  const machine = useMachine(voceditMachine({ resourceManager, fileHandle: null }))
   provide(VOCEDIT_MACHINE_KEY, machine)
 
   return machine
