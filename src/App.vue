2f5da420306642baa2a9c39c180dcc19163062a4
Edmond Chuc <edmond@kurrawong.ai>
2025-09-01 14:37:32 +1000

    chore: initial integration of shacl-ui

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { RouterView } from 'vue-router'
 import { createResourceManager } from '@kurrawongai/shacl-ui'
 import { createVocEditMachine } from '@/composables/vocedit-machine'
 import AppMenubar from '@/components/AppMenubar.vue'
 import Main from '@/components/Main.vue'
+import vocpub from '@/assets/vocpub.ttl?raw'
 
 const resourceManager = createResourceManager()
-const { snapshot } = createVocEditMachine(resourceManager)
-const state = computed(() => snapshot.value)
+resourceManager.resetShapesGraph(vocpub)
+createVocEditMachine(resourceManager)
 </script>
 
 <template>
   <Main>
     <template #header>
       <AppMenubar />
-      <div class="px-5 py-3">
-        <pre>{{ state.context.resourceManager.dataGraph.value.size }}</pre>
-        <pre>{{ state.value }}</pre>
-      </div>
     </template>
 
     <RouterView />
--- src/views/ResourceView.vue
+++ src/views/ResourceView.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
+import { computed, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useVocEditMachine } from '@/composables/vocedit-machine'
 import { useRouter } from 'vue-router'
+import n3 from 'n3'
+import { ResourceShell } from '@kurrawongai/shacl-ui'
 
 const route = useRoute()
 const iri = route.query.iri as string
 const router = useRouter()
+const { namedNode } = n3.DataFactory
 
 const { snapshot } = useVocEditMachine()
 
-// if we are not in the opened state, we should redirect to the home page
-if (!snapshot.value.matches('opened') || !iri) {
-  router.push('/')
-}
+watch(
+  () => snapshot.value.matches('opened'),
+  (matches) => {
+    console.log('matches', matches)
+    if (!matches || !iri) {
+      router.push('/')
+    }
+  },
+  { immediate: true },
+)
+
+// Get the class types of the resource and use it to render the correct view.
+const focusNode = computed(() => namedNode(iri))
+const nodeShape = computed(() => {
+  // if class type is a concept scheme
+  return namedNode('https://linked.data.gov.au/def/vocpub/validator/Shui-ConceptScheme')
+})
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold">This is an resource page</h1>
+  <div class="px-6">
+    <ResourceShell :focus-node="focusNode" :node-shape="nodeShape" />
+  </div>
 </template>
