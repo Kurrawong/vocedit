6e82b3b2b0c5755340e00e3a68bf7832acd80773
Edmond Chuc <edmond@kurrawong.ai>
2025-09-02 14:29:12 +1000

    chore: conditionally display home page text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+import { useVocEditMachine } from '@/composables/vocedit-machine'
+
+const { snapshot } = useVocEditMachine()
+</script>
+
 <template>
   <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
     <div
       class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min flex items-center justify-center"
     >
-      <span class="text-sm text-muted-foreground italic">Use the menu bar to get started</span>
+      <span v-if="!snapshot.matches('opened')" class="text-sm text-muted-foreground italic"
+        >Use the menu bar to get started</span
+      >
+      <span v-else class="text-sm text-muted-foreground italic">
+        Select a vocabulary, collection, or concept to view it
+      </span>
     </div>
   </div>
 </template>
